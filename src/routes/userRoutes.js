44ec5dab29ad27616a37eea0ed3bbb5ee6e74847
Nick <nickbrimmer@gmail.com>
2020-02-17 15:47:02 -0800

    getting rid of authorization

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,40 +1,35 @@
 const { Router } = require('express');
-const ensureAuth = require('../middleware/ensure-auth');
 const User = require('../models/User');
-
-const MAX_AGE_IN_MS = 24 * 60 * 60 * 1000;
-
-const setSessionCookie = (res, token) => {
-  res.cookie('session', token, {
-    maxAge: MAX_AGE_IN_MS
-  });
-};
 
 
 module.exports = Router()
   .get('/signup', (req, res, next) => {
     User
       .create(req.body)
-      .then(user => {
-        setSessionCookie(res, user.AuthToken());
-        res.send(user);
-      })
+      .then(user => res.send(user))
       .catch(next);
   })
   .post('/login', (req, res, next) => {
     User
       .authorize(req.body)
-      .then(user => {
-        setSessionCookie(res, user.AuthToken());
-        res.send(user);
-      })
+      .then(user => res.send(user))
       .catch(next);
   })
-  .post('./logout', (req, res) => {
-    res.clearCookie('session', {
-      MAX_AGE_IN_MS
-    });
+  .get('/:id', (req, res, next) => {
+    User
+      .findById(req.params.id)
+      .then(movie => res.send(movie))
+      .catch(next);
   })
-  .get('verify', ensureAuth, (req, res) => {
-    res.send(req.user);
+  .patch('/:id', (req, res, next) => {
+    User
+      .findByIdAndUpdate(req.params.id, req.body, { new: true })
+      .then(movie => res.send(movie))
+      .catch(next);
+  })
+  .delete('/:id', (req, res, next) => {
+    User
+      .findByIdAndDelete(req.params.id)
+      .then(movie => res.send(movie))
+      .catch(next);
   });
